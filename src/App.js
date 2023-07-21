9e62cc21c07de198ee5b3c96518d47b74f812bc2
Nhlakanipho <madnhlaka@gmail.com>
2023-07-21 12:25:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Navbar from './components/Navbar';
+// import Navbar from "./components/Navbar.js  "
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+     <Navbar/>
+  
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Navbar.css"
+
+
+const Navbar = () => {
+  return (
+
+    <div>
+      <div className='nav'>
+      <h1>mr price</h1>
+      <input  placeholder="Seach Item,]"/>
+      <a href="#">Sing in / Register</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
